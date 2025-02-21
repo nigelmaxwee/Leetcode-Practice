0332e7b8c7269fc50b6f7e67568a2d0e29a6cd6d
Nigel Wee <nigelmaxwee@gmail.com>
2025-02-21 22:24:50 +0800

    Update 1378

--- SQL 50/1378. Replace Employee ID With The Unique Identifier.sql
+++ SQL 50/1378. Replace Employee ID With The Unique Identifier.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT EmployeeUNI.unique_id, Employees.name
 FROM Employees
-LEFT JOIN EmployeeUNI on Employees.id = EmployeeUNI.id
+LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
