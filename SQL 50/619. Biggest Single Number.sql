22b5756377bb1da351dafd21415fcb59ef215a33
Nigel Wee <nigelmaxwee@gmail.com>
2025-02-24 22:36:18 +0800

    619

---
+++ SQL 50/619. Biggest Single Number.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num)<2)
