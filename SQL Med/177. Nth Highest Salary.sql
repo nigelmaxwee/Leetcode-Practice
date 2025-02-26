5559f9aeaaf56f26511f8df5572cb43319ab8c35
Nigel Wee <nigelmaxwee@gmail.com>
2025-02-26 21:38:22 +0800

    177

---
+++ SQL Med/177. Nth Highest Salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE X INT; # Semi-colons needed within begin-end statements to terminate
+SET X = N-1; # OFFSET function does not take arithmetic operations 
+  RETURN (
+      # Write your MySQL query statement below.
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET X
+  );
+END
