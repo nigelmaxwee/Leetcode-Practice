83196f3460ed2fbf24604ae1e35ff953cfb6d9c4
Nigel Wee <nigelmaxwee@gmail.com>
2025-02-26 21:30:11 +0800

    Update 176

--- SQL Med/176. Second Highest Salary.sql
+++ SQL Med/176. Second Highest Salary.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
-(SELECT DISTINCT salary 
+(SELECT DISTINCT salary  # Distinct allows us to compare for when theres matching top salaries
 FROM Employee 
 ORDER BY salary DESC 
 LIMIT 1 OFFSET 1) AS SecondHighestSalary;
