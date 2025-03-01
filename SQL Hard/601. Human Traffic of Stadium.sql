ed90d5a9e7f5319c9c44b3f13bddd5f8ca3f811c
Nigel Wee <nigelmaxwee@gmail.com>
2025-03-01 19:40:50 +0800

    601

---
+++ SQL Hard/601. Human Traffic of Stadium.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+WITH ConsecutiveStadium AS (
+    SELECT id AS id1, visit_date, people AS people1,
+           LEAD(id, 1) OVER (ORDER BY id) AS id2,
+           LEAD(id, 2) OVER (ORDER BY id) AS id3,
+           LEAD(people, 1) OVER (ORDER BY id) AS people2,
+           LEAD(people, 2) OVER (ORDER BY id) AS people3
+    FROM Stadium
+)
+SELECT id, visit_date, people
+FROM Stadium
+WHERE id IN(
+    SELECT id1 
+    FROM ConsecutiveStadium
+    WHERE people1 >= 100 AND people2 >= 100 AND people3 >= 100
+    UNION
+    SELECT id2
+    FROM ConsecutiveStadium
+    WHERE people1 >= 100 AND people2 >= 100 AND people3 >= 100
+    UNION
+    SELECT id3
+    FROM ConsecutiveStadium
+    WHERE people1 >= 100 AND people2 >= 100 AND people3 >= 100  
+)
+ORDER BY visit_date
